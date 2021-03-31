b103adc247e5fe1de6fd89b03962e7498b2234cf
fawad60 <fawadmalik60@gmail.com>
2021-03-31 23:20:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+ My personal Porfolio Website
---
+++ js/index.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 60,
+  backSpeed: 40,
+  smartBackspace: true, // this is a default
+  cursorChar: "|",
+  autoInsertCss: true,
+});
